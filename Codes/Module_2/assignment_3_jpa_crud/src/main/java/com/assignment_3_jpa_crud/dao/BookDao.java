73d7da360de75530bf382628e1d8892d7276453a
Kashish Juneja <VE00YN351@YMI.COM>
2026-01-25 19:15:33 +0530

    Assignment 3 Module 2 Done

---
+++ Codes/Module_2/assignment_3_jpa_crud/src/main/java/com/assignment_3_jpa_crud/dao/BookDao.java
@@ -0,0 +1,19 @@
+package com.assignment_3_jpa_crud.dao;
+
+import java.util.List;
+
+import com.assignment_3_jpa_crud.entity.Book;
+
+public interface BookDao {
+	public void addBook(Book book);
+
+	public void deleteBook(Book book);
+	
+	public void deleteBookByIsbn(String isbn);
+	
+	public Book getBookById(int id);
+
+	public Book getBookByIsbn(String isbn);
+	
+	public List<Book> getAllBooks(); 
+}
---
+++ Codes/Module_2/assignment_3_jpa_crud/src/main/java/com/assignment_3_jpa_crud/util/JpaUtil.java
@@ -0,0 +1,21 @@
+package com.assignment_3_jpa_crud.util;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public final class JpaUtil {
+	private static final EntityManagerFactory emf;
+	
+	static {
+		emf = Persistence.createEntityManagerFactory("jpa-unit");
+	}
+	
+	public static EntityManager getEntityManager() {
+		return emf.createEntityManager();
+	}
+	
+	public void shutDown() {
+		emf.close();
+	}
+}
